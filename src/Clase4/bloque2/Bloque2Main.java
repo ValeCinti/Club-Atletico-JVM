88626a690d073d737fee62d58dd30191b29c3729
ValeCinti <valentino09022007@gmail.com>
2026-04-09 17:13:55 -0300

    Merge manual desde main

--- src/Clase4/bloque2/Bloque2Main.java
+++ src/Clase4/bloque2/Bloque2Main.java
@@ -1,34 +1,16 @@
 package clase4.bloque2;
-import Implementaciones.ColaEstatica;
-import Interfaces.ColaTDA;
+import Implementaciones.*;
+import Interfaces.*;
 
 public class Bloque2Main {
     public static void main(String[] args) {
-        ColaTDA cola = new ColaEstatica();
+        ColaEstaticaa colaE = new ColaEstaticaa();
+        ColaPrioridadEstatica COLA = new ColaPrioridadEstatica();
+        System.out.println("=== Bloque 2: Pilas (FIFO) ===\n");
+        ImpresoraLab.ejecutar(colaE);
+        FilaCajero.ejecutar(COLA);
 
-        cola.InicializarCola();
 
-        System.out.println("¿Cola vacía?: " + cola.ColaVacia());
 
-        cola.Acolar(10);
-        cola.Acolar(20);
-        cola.Acolar(30);
-
-        System.out.println("Primero: " + cola.Primero()); // 10
-
-        cola.Desacolar();
-        System.out.println("Primero luego de desacolar: " + cola.Primero()); // 20
-
-        cola.Acolar(40);
-        System.out.println("Primero luego de acolar 40: " + cola.Primero()); // 20
-
-        cola.Desacolar();
-        System.out.println("Primero luego de desacolar: " + cola.Primero()); // 30
-
-        cola.Desacolar();
-        System.out.println("Primero luego de desacolar: " + cola.Primero()); // 40
-
-        cola.Desacolar();
-        System.out.println("¿Cola vacía?: " + cola.ColaVacia()); // true
     }
 }
