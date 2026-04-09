aa7e8d89164a8e29be5a58bd72927f2820b62391
Gonzalorag <graggio.ar@gmail.com>
2026-04-09 16:57:56 -0300

    FilaCajero avance full serio re nashe :v:v

--- Clase 4/Bloque 2/src/Bloque2Main.java
+++ Clase 4/Bloque 2/src/Bloque2Main.java
@@ -3,7 +3,12 @@ import Implementacion.*;
 public class Bloque2Main {
     public static void main(String[] args) {
         ColaEstaticaa colaE = new ColaEstaticaa();
+        ColaPrioridadEstatica COLA = new ColaPrioridadEstatica();
         System.out.println("=== Bloque 2: Pilas (FIFO) ===\n");
         ImpresoraLab.ejecutar(colaE);
+        FilaCajero.ejecutar(COLA);
+
+
+
     }
 }
--- Clase 4/Bloque 2/src/Implementacion/FilaCajero.java
+++ Clase 4/Bloque 2/src/Implementacion/FilaCajero.java
@@ -1,5 +1,29 @@
 package Implementacion;
 
-public class FilaCajero {
+import Bloque2Interfaces.ColaPrioridadEstatica;
+public class FilaCajero{
+    public static int PrioridadPersona = 1;
+    public static int PrioridadSeñoraEmbarazada = 3;
+    public static int PrioridadJubilado = 5;
+    //OrdenDeLLEGADA
+    public static int Persona1 = 1;
+    public static int SeñoraEmbarazada = 2;
+    public static int Jubilado = 3;
+    public static int Persona2 = 4;
     
+    public static void ejecutar(ColaPrioridadEstatica COLA){
+        COLA.InicializarColaPrioridad();
+        System.out.println("primero llega una persona, se mete a la fila");
+        COLA.AcolarPrioridad(Persona1,PrioridadPersona);
+        System.out.println("El primero es persona1");
+        System.out.println("primero llega una señora embarazada, se mete a la fila");
+        COLA.AcolarPrioridad(SeñoraEmbarazada,PrioridadSeñoraEmbarazada);
+        System.out.println("primero llega un Jubilado, se mete a la fila");
+        COLA.AcolarPrioridad(Jubilado, PrioridadJubilado);
+        System.out.println("primero llega otra persona, se mete a la fila");
+
+        
+        
+        COLA.Desacolar();
+    }
 }
