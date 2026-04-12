16e78acbecf42fcd00dbf5d6192b088f1415328d
Mendez Milena <mile.lu.mendez@gmail.com>
2026-04-12 17:33:16 -0300

    primer ejercicio bloque 3

---
+++ src/Clase4/Bloque3/PadronElectoral.java
@@ -0,0 +1,35 @@
+package Clase4.Bloque3;
+import Implementaciones.*;
+import Interfaces.*;
+
+public class PadronElectoral {
+    static void main(String[] args) {
+        System.out.println("inicializo el nuevo conjunto llamado Padron");
+        ConjuntoTDA Padron = new ConjuntoEstatico();
+        Padron.InicializarConjunto();
+        System.out.print("ingresamos el dni 1234 para probar");
+        int DniParaVotar =1234;
+        if(!Padron.Pertenece(DniParaVotar)){
+            Padron.Agregar(DniParaVotar);
+            System.out.println("Voto registrado con exito para el DNI"+DniParaVotar);
+        } else {
+            System.out.println("ERROR: El DNI " +DniParaVotar + "ya emitio su voto");
+        }
+        DniParaVotar =5678;
+        System.out.print("ingresamos el dni 5678");
+
+        if(!Padron.Pertenece(DniParaVotar)){
+            Padron.Agregar(DniParaVotar);
+            System.out.println("Voto registrado con exito para el DNI"+DniParaVotar);
+        } else {
+            System.out.println("ERROR: El DNI " +DniParaVotar + "ya emitio su voto");
+        }
+        System.out.println("ahora intentamos volver a ingresar un DNI que ya voto");
+        if(!Padron.Pertenece(DniParaVotar)){
+            Padron.Agregar(DniParaVotar);
+            System.out.println("Voto registrado con exito para el DNI"+DniParaVotar);
+        } else {
+            System.out.println("ERROR: El DNI " +DniParaVotar + "ya emitio su voto");
+        }
+    }
+}
