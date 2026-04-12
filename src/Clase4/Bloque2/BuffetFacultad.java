aa3e1c2fdc2847280b17782cd82e1db9d04c7208
Gonzalorag <graggio.ar@gmail.com>
2026-04-12 16:07:26 -0300

    ColaBuffet creacion de archivo

---
+++ src/Clase4/Bloque2/BuffetFacultad.java
@@ -0,0 +1,19 @@
+package Clase4.Bloque2;
+
+import Interfaces.ColaTDA;
+
+public class BuffetFacultad {
+
+    public static void ejecutar(ColaTDA ColaBuffet){
+        
+
+
+
+    }
+
+
+
+
+
+    
+}
