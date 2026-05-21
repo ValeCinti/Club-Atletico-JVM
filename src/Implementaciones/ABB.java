10d1197ab3a5b9bd0af93d39c01c4d25e2976396
ValeCinti <valentino09022007@gmail.com>
2026-05-21 15:25:17 -0300

    ArbolTDA parte 5

--- src/Implementaciones/ABB.java
+++ src/Implementaciones/ABB.java
@@ -1,38 +1,73 @@
 package Implementaciones;
 
-public class ABB {
+import Interfaces.ABBTDA;
 
-    public int raiz(){
-        return 0;
+public class ABB implements ABBTDA {
+
+    private NodeABB nodo;
+
+    @Override
+    public int raiz() {
+        return nodo.info;
     }
 
-    public ABB hijoIzq(){
-
+    @Override
+    public ABBTDA hijoIzq() {
+        return nodo.HijoIzq;
     }
 
-    public ABB hijoDer(){
-
+    @Override
+    public ABBTDA hijoDer() {
+        return nodo.HijoDer;
     }
 
+    @Override
     public void agregarElem(int x) {
-        if(nodo == null) {
-            nodo = new NodoABB();
+        if (nodo == null) {
+            nodo = new NodeABB();
             nodo.info = x;
             nodo.HijoIzq = new ABB();
             nodo.HijoDer = new ABB();
-        }
-        else if(nodo.info > x) {
+        } else if (nodo.info > x) {
             nodo.HijoIzq.agregarElem(x);
-        }
-        else if(nodo.info < x) {
+        } else if (nodo.info < x) {
             nodo.HijoDer.agregarElem(x);
         }
     }
 
-    public void eliminarElem(int x){
+    @Override
+    public void eliminarElem(int x) {
+        if (nodo != null) {
+            if (nodo.info == x) {
+                // Caso 1: Es una hoja (no tiene hijos)
+                if (nodo.HijoIzq.arbolVacio() && nodo.HijoDer.arbolVacio()) {
+                    nodo = null;
+                }
+                // Caso 2: Tiene solo hijo derecho
+                else if (nodo.HijoIzq.arbolVacio() && !nodo.HijoDer.arbolVacio()) {
+                    nodo = ((ABB) nodo.HijoDer).nodo; // "Puenteamos" el nodo actual
+                }
+                // Caso 3: Tiene solo hijo izquierdo
+                else if (!nodo.HijoIzq.arbolVacio() && nodo.HijoDer.arbolVacio()) {
+                    nodo = ((ABB) nodo.HijoIzq).nodo;
+                }
+                // Caso 4: Tiene ambos hijos
+                else {
+                    // Buscamos el mayor de los menores (el mayor del subárbol izquierdo)
+                    int mayorIzq = buscarMayor(nodo.HijoIzq);
+                    nodo.info = mayorIzq; // Reemplazamos el valor
+                    nodo.HijoIzq.eliminarElem(mayorIzq); // Eliminamos el nodo duplicado abajo
+                }
+            } else if (nodo.info > x) {
+                nodo.HijoIzq.eliminarElem(x); // Buscamos a la izquierda
+            } else if (nodo.info < x) {
+                nodo.HijoDer.eliminarElem(x); // Buscamos a la derecha
+            }
+        }
     }
 
-    boolean arbolVacio(){
-        return true;
+    @Override
+    public boolean arbolVacio() {
+        return nodo == null;
     }
 }
---
+++ src/Implementaciones/NodeABB.java
@@ -0,0 +1,9 @@
+package Implementaciones;
+
+import Interfaces.ABBTDA;
+
+public class NodeABB {
+    int info;
+    ABBTDA HijoIzq;
+    ABBTDA HijoDer;
+}
